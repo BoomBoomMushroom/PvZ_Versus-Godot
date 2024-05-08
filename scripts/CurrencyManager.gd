c83a5965f598c1350172128df6b9f62d72a5fd79
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2024-05-08 08:25:15 -0400

    Fixed the stupid spawn currency error!

--- scripts/CurrencyManager.gd
+++ scripts/CurrencyManager.gd
@@ -9,6 +9,9 @@ var random
 @onready var currency_manager = %CurrencyManager
 const CURRENCY_DROP_PREFAB = preload("res://scenes/currency_drop.tscn")
 
+const SUN_DROP_SPRITE = preload("res://assets/plants/sun_drop.png")
+const ZOMBIE_DROP_SPRITE = preload("res://assets/zombie/zombie_drop.png")
+
 func _ready():
 	random = RandomNumberGenerator.new()
 	random.randomize()
@@ -28,6 +31,14 @@ func spawnDrop(teamName, value):
 	newDrop.position.y = 0
 	newDrop.name = teamName + "_" + str(value)
 	newDrop.currency_manager = currency_manager
+	
+	newDrop.amount = value
+	newDrop.isTeam1 = teamName.to_lower() == "team1"
+	
+	var newTexture = SUN_DROP_SPRITE
+	if teamName.to_lower() == "team2": newTexture = ZOMBIE_DROP_SPRITE
+	
+	newDrop.get_node("Sprite2D").texture = newTexture
 	add_child(newDrop)
 
 
--- scripts/currency_drop.gd
+++ scripts/currency_drop.gd
@@ -5,21 +5,24 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var fallSpeed = .01;
 
 var currency_manager
+var didReady = false
 @onready var sprite_2d = $Sprite2D
 
-const SUN_DROP_SPRITE = preload("res://assets/plants/sun_drop.png")
-const ZOMBIE_DROP_SPRITE = preload("res://assets/zombie/zombie_drop.png")
+var amount = 50
+var isTeam1 = true
 
 func _ready():
 	var nameLower = name.to_lower()
-	sprite_2d = $Sprite2D
 	
 	if "team1" in nameLower:
-		sprite_2d.texture = SUN_DROP_SPRITE
+		pass
+		#sprite_2d.texture = SUN_DROP_SPRITE
 	elif "team2" in nameLower:
-		sprite_2d.texture = ZOMBIE_DROP_SPRITE
+		pass
+		#sprite_2d.texture = ZOMBIE_DROP_SPRITE
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y = fallSpeed * gravity
@@ -33,9 +36,12 @@ func _on_collect_body_entered(body):
 	# X = team number
 	# Y = amount to give
 	
+	"""
 	var isTeam1 = false
 	var nameSplits = name.split("_")
 	if nameSplits[0].to_lower() == "team1": isTeam1 = true
+	"""
+	
 	var correctPicker = false
 	
 	if isTeam1 and "team1" in body.name.to_lower():
@@ -45,7 +51,7 @@ func _on_collect_body_entered(body):
 	
 	if correctPicker == false: return
 	
-	var amount = int(nameSplits[1])
+	#var amount = int(nameSplits[1])
 	
 	currency_manager.collectMoney(isTeam1, amount)
 	queue_free()
