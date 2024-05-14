3cb7a4a14305649762aeef37c89e65411e489b0f
BoomBoomMushroom <dillioncat7@gmail.com>
2024-05-14 15:33:11 -0400

    Working lawnmowers!

--- scripts/LawnMower.gd
+++ scripts/LawnMower.gd
@@ -1,16 +1,22 @@
-extends Area2D
+extends CharacterBody2D
 
-var speed = 1
+var speed = 40
 var triggered = false
+var maxX = 0
 
 func _on_area_2d_body_entered(body):
-	triggered = true
-	print(body.has_meta("zombieLoad"))
+	if body.has_meta("zombieLoad"):
+		triggered = true
+		body.health = 0
 
 func _ready():
 	triggered = false
 
-
 func _process(delta):
+	if position.x >= maxX: queue_free()
+
+func _physics_process(delta):
 	if triggered == false: return
+	velocity.x = speed
 	
+	move_and_slide()
